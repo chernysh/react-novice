d6115f8c002860e7db07cc5e62c854abd359872c
Vadim Chernysh <chernysh.vadim@gmail.com>
2015-08-04 10:44:32 -0400

    Added <img> events as example

--- lib/components/about.jsx
+++ lib/components/about.jsx
@@ -7,11 +7,16 @@ var Link = Router.Link;
     
 var About = React.createClass({
 
+    onErrorImgHandler: function(e) {
+        var errorTextElement = document.createTextNode("Could not load image file");
+        e.target.parentNode.appendChild(errorTextElement);
+    },
     render: function() {
         return  (
             <div>
                 <div className="wrap">
                     <div className="column">
+                        <p><img onError={this.onErrorImgHandler} src="build/images/about.png" /></p>
                         <p>About page</p>
                     </div>
                 </div>
